92ab673d74a647639dadfa6a4c7267624f6531b9
HerListo21 <166368184+HerListo21@users.noreply.github.com>
2024-12-12 20:27:33 +0100

    Update principal.js
    Ahora también extrae el ID del usuario, la url a su perfil

--- Codigo/PluGin/principal.js
+++ Codigo/PluGin/principal.js
@@ -3,11 +3,17 @@ function obtenerUsuario() {
     return userName;
   }
   function obtenerIdentificador() {
-    let userID = document.getElementsByClassName("text-username").innerHTML;
-    return userID;
+    let usernameElement = document.querySelector('.text-username');
+    if (usernameElement) {
+      return usernameElement.getAttribute('href');
+    } else {
+      console.error("No se encontró el elemento con la clase 'text-username'");
+      return null;
+    }
   }
 
 console.log(obtenerUsuario());
+console.log(obtenerIdentificador());
 
 function saveUserName(userName) {
   chrome.storage.local.set({ userName: userName }, function() {
@@ -15,7 +21,14 @@ function saveUserName(userName) {
   });
 }
 
+function saveID(ID) {
+  chrome.storage.local.set({ ID: ID }, function() {
+    console.log('ID de usuario guardado:', ID);
+  });
+}
+
 saveUserName(obtenerUsuario());
+saveID(obtenerIdentificador());
 
 // Función para obtener el userName desde chrome.storage
 function getUserName() {
@@ -28,6 +41,17 @@ function getUserName() {
     }
   });
 }
+function getID() {
+  chrome.storage.local.get('ID', function(result) {
+    if (result.ID) {
+      console.log('ID de usuario recuperado:', result.ID);
+      // Aquí puedes usar el valor de userName según lo necesites
+    } else {
+      console.log('No se ha encontrado la ID.');
+    }
+  });
+}
 
 // Llamar a la función para recuperar el nombre de usuario
 getUserName();
+getID();
