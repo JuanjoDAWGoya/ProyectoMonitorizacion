f1073c8a325c185ac1b621fbbc0e8ad987f1f474
HerListo21 <166368184+HerListo21@users.noreply.github.com>
2024-12-12 14:03:20 +0100

    Update background.js
    Actualizo el background para que extraiga tanto el nombre de usuario de aulavirtual como las URLs visitadas

--- Codigo/PluGin/background.js
+++ Codigo/PluGin/background.js
@@ -1,1 +1,25 @@
+// background.js
 
+// Escuchar cuando se carga una nueva página
+chrome.webNavigation.onCompleted.addListener(function (details) {
+    const url = details.url;
+    console.log("Página visitada:", url);
+  
+    // Recuperamos el userName y el historial de URLs desde chrome.storage
+    chrome.storage.local.get(['userName', 'visitedUrls'], function(result) {
+      const userName = result.userName || "Desconocido";  // Si no hay userName, se usa "Desconocido"
+      const visitedUrls = result.visitedUrls || [];
+  
+      // Agregamos la URL visitada al historial junto con el userName
+      visitedUrls.push({
+        url: url,
+        user: userName,
+        timestamp: new Date().toISOString()
+      });
+  
+      // Guardamos el historial de URLs actualizado
+      chrome.storage.local.set({ visitedUrls: visitedUrls }, function() {
+        console.log("URL registrada correctamente con el nombre de usuario.");
+      });
+    });
+  }, {});
