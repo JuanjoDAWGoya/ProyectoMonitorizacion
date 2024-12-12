efbbdf0114bf50aabe183179df5c96b0aea501e9
HerListo21 <166368184+HerListo21@users.noreply.github.com>
2024-12-12 19:09:24 +0100

    Update principal.js
    Almacena el nombre de usuario y su NRE

--- Codigo/PluGin/principal.js
+++ Codigo/PluGin/principal.js
@@ -1,4 +1,33 @@
 function obtenerUsuario() {
-  let userName = document.getElementById("actionmenuaction-1").innerHTML;
-  return userName;
+    let userName = document.getElementById("actionmenuaction-1").innerHTML;
+    return userName;
+  }
+  function obtenerIdentificador() {
+    let userID = document.getElementsByClassName("text-username").innerHTML;
+    return userID;
+  }
+
+console.log(obtenerUsuario());
+
+function saveUserName(userName) {
+  chrome.storage.local.set({ userName: userName }, function() {
+    console.log('Nombre de usuario guardado:', userName);
+  });
 }
+
+saveUserName(obtenerUsuario());
+
+// Función para obtener el userName desde chrome.storage
+function getUserName() {
+  chrome.storage.local.get('userName', function(result) {
+    if (result.userName) {
+      console.log('Nombre de usuario recuperado:', result.userName);
+      // Aquí puedes usar el valor de userName según lo necesites
+    } else {
+      console.log('No se ha encontrado el nombre de usuario.');
+    }
+  });
+}
+
+// Llamar a la función para recuperar el nombre de usuario
+getUserName();
